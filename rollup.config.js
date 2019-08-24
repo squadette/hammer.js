b6ddc1a8bdebb486ca33b542f46c3b43699294aa
Alexey Mahotkin <squadette@gmail.com>
2019-08-24 22:13:18 +0200

    Fix up, no dist for now

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import babel from 'rollup-plugin-babel';
 export default {
   input: 'src/main.js',
   output: {
-    file: 'dist/hammer.js',
+    file: 'hammer.js',
     format: 'es',
     intro: " (function(window, document, exportName, undefined) { \n'use strict';",
     outro: "})(window, document, 'Hammer');"
